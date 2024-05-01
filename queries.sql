a3b630a91b4a292b389aa3e0c338813136c52ae9
Vazlevski <98548301+Vazlevski@users.noreply.github.com>
2024-05-01 20:01:04 +0400

    Add files via upload
    Первый отчет о десятке лучших продавцов. Таблица состоит из трех колонок - данных о продавце, суммарной выручке с проданных товаров и количестве проведенных сделок, и отсортирована по убыванию выручки.
    
    Второй отчет содержит информацию о продавцах, чья средняя выручка за сделку меньше средней выручки за сделку по всем продавцам. Таблица отсортирована по выручке по возрастанию.
    
    Третий отчет содержит информацию о выручке по дням недели. Каждая запись содержит имя и фамилию продавца, день недели и суммарную выручку. Отсортируйте данные по порядковому номеру дня недели и seller.
    
    Все данные округлены до целых чисел.
    
    /
    
    The first report on the top ten sellers. The table consists of three columns - data about the seller, total revenue from sold goods and the number of conducted transactions, and is sorted by descending revenue.
    
    The second report contains information about sellers whose average revenue per transaction is less than the average revenue per transaction for all sellers. The table is sorted by revenue in ascending order.
    
    The third report contains information about revenue by day of the week. Each record contains the salesperson's first and last name, day of the week, and total revenue. Sort the data by day of the week and seller.
    
    All data is rounded to whole numbers.

--- queries.sql
+++ queries.sql
@@ -1,1 +1,32 @@
-select count(customer_id) as customers_count from public.customers;
+select concat(e.first_name, ' ', e.last_name) as seller, count(s.sales_id), sum(round(s.quantity * p.price, 0)) as income
+from public.sales s
+join employees e 
+on e.employee_id = s.sales_person_id 
+join products p 
+on p.product_id = s.product_id
+group by 1
+order by income desc
+limit 10;
+
+with a as (select concat(e.first_name, ' ', e.last_name) as seller, sum(s.quantity * p.price) as income
+from public.sales s
+join employees e 
+on e.employee_id = s.sales_person_id 
+join products p 
+on p.product_id = s.product_id
+group by 1), b as (select avg(income) as avg_total from a),
+c as (select seller, avg(income) as avg_sale from a group by 1)
+select seller, round(avg_sale, 0) as average_income
+from c 
+cross join b 
+where avg_sale < avg_total
+order by average_income asc;
+
+select concat(e.first_name, ' ', e.last_name) as seller, to_char(sale_date, 'Day') AS day_of_week,  sum(round(s.quantity * p.price, 0)) as income
+from public.sales s
+join employees e 
+on e.employee_id = s.sales_person_id 
+join products p 
+on p.product_id = s.product_id
+group by 1, 2
+order by extract(ISODOW from max(s.sale_date)), 1;
